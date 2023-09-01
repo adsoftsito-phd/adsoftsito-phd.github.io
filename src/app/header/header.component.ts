f36d304c6b58bb5ee2a4ebade98ca2f005241e8d
adsoftsito-phd <acenteno@uv.mx>
2023-09-01 10:27:04 -0600

    add vars header #3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+ nombre : string = "fake name";
+ titulo: string = "fake PhD";
+ objetivo: string = "fake goal";
+ foto: string = "fake photo";
+ email: string = "fake email";
+ celular: string = "fake cel";
+ ubicacion: string = "fake city";
+ redsocial: string = "fake social"; 
+
 }
