992377ed233bb029fe632a76be3c66f50da82d94
adsoftsito-phd <120610355+adsoftsito-phd@users.noreply.github.com>
2023-09-05 11:45:58 -0600

    Header Closes #3 (#4)
    * create mockup header #3
    
    * add vars header #3
    
    * create header cv #3
    
    * fix test #3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+ constructor(private headerService : HeaderService) 
+ {
+ }
+
+ nombre : string = "fake name";
+ titulo: string = "fake PhD";
+ objetivo: string = "fake goal";
+ foto: string = "fake photo";
+ email: string = "fake email";
+ celular: string = "fake cel";
+ ubicacion: string = "fake city";
+ redsocial: string = "fake social"; 
+
+ ngOnInit() : void {
+   this.headerService.getHeader()
+   .subscribe( (data: any) => {
+     console.log(data);
+    // alert(data);
+      this.nombre = data.name;
+
+   });
+ }
 
 }
